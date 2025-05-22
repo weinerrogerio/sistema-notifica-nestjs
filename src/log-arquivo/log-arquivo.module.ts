f5c8ee0cfd2816d91918c901c202d62cc6d69142
weinerrogerio <weinerrogerio@gmail.com>
2025-05-22 20:59:38 -0300

    substituição do modulo LogArquivoModule para  LogArquivoImportModule para log de arquivo

--- src/log-arquivo/log-arquivo.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LogArquivoService } from './log-arquivo.service';
-import { LogArquivoController } from './log-arquivo.controller';
-
-@Module({
-  controllers: [LogArquivoController],
-  providers: [LogArquivoService],
-})
-export class LogArquivoModule {}
