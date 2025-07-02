554cd9f8077a9d6b46a4e7e893a5b20ccbb58f78
weinerrogerio <weinerrogerio@gmail.com>
2025-07-02 15:45:37 -0300

    adição de create em log-arquivo-import para logs de upload de arquivo

--- src/log-arquivo-import/dto/create-log-arquivo-import.dto.ts
+++ src/log-arquivo-import/dto/create-log-arquivo-import.dto.ts
@@ -1,1 +1,37 @@
-export class CreateLogArquivoImportDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { StatusImportacao } from '../enum/log-arquivo.enum';
+
+export class CreateLogArquivoImportDto {
+  @IsString()
+  @IsNotEmpty()
+  nome_arquivo: string;
+
+  @IsString()
+  mimetype: string;
+
+  @IsNumber()
+  tamanho_arquivo?: number; // em bytes
+
+  @IsString()
+  status: StatusImportacao;
+
+  @IsNumber()
+  total_registros?: number;
+
+  @IsNumber()
+  registros_processados?: number;
+
+  @IsNumber()
+  registros_com_erro?: number;
+
+  @IsString()
+  detalhes_erro?: string; // JSON com detalhes dos erros
+
+  @IsString()
+  duracao?: string; // formato: "00:01:23"
+
+  // Relacionamento com usuário
+  @IsNumber()
+  @IsNotEmpty()
+  fk_usuario: number;
+}
