be8164eda412cd42eed465f12054ece9fc656de1
weinerrogerio <weinerrogerio@gmail.com>
2026-03-12 09:39:59 -0300

    fix: add guards e roles a config

--- src/config/config.controller.ts
+++ src/config/config.controller.ts
@@ -1,7 +1,20 @@
-import { Controller, Get, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { PersonalConfigService } from './config.service';
 import { UpdateConfigDto } from './dto/update-config.dto';
+import { AuthTokenGuard } from '@app/auth/guards/auth-token.guard';
+import { RolesGuard } from '@app/auth/guards/roles.guard';
+import { Roles } from '@app/auth/decorators/roles.decorator';
+import { Role } from '@app/common/enums/role.enum';
 
+@UseGuards(AuthTokenGuard, RolesGuard)
 @Controller('config')
 export class ConfigController {
   constructor(private readonly personalConfigService: PersonalConfigService) {}
@@ -12,25 +25,25 @@ export class ConfigController {
   } */
 
   @Get()
+  @Roles(Role.ADMIN)
   findAll() {
     return this.personalConfigService.findAll();
   }
 
   @Get(':id')
+  @Roles(Role.ADMIN)
   findOne(@Param('id') id: string) {
     return this.personalConfigService.findOne(+id);
   }
-  /* 
-  @Patch(':id')
-  update(@Param('id') id: '1', @Body() updateConfigDto: UpdateConfigDto) {
-    return this.personalConfigService.update(+id, updateConfigDto);
-  } */
+
   @Patch()
+  @Roles(Role.ADMIN)
   update(@Body() updateConfigDto: UpdateConfigDto) {
     return this.personalConfigService.update(updateConfigDto);
   }
 
   @Delete(':id')
+  @Roles(Role.ADMIN)
   remove(@Param('id') id: string) {
     return this.personalConfigService.remove(+id);
   }
